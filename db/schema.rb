9bee99d7e7f8c069bf772224588317f6332d1227
DanL12186 <danlrcn@hotmail.com>
2018-11-10 17:37:01 -0500

    added "site_note" column to cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_195744) do
+ActiveRecord::Schema.define(version: 2018_11_10_211638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_11_02_195744) do
     t.text "flavor_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "site_note"
     t.index ["abilities"], name: "index_cards_on_abilities", using: :gin
     t.index ["activated_abilities"], name: "index_cards_on_activated_abilities", using: :gin
     t.index ["colors"], name: "index_cards_on_colors", using: :gin
