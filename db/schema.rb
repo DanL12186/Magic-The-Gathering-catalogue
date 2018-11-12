14b1cfcdbb20c86927a0e67a82440e277b4e3c8c
DanL12186 <danlrcn@hotmail.com>
2018-11-12 13:04:53 -0500

    +color table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_10_211638) do
+ActiveRecord::Schema.define(version: 2018_11_12_024631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_11_10_211638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "site_note"
+    t.string "color"
     t.index ["abilities"], name: "index_cards_on_abilities", using: :gin
     t.index ["activated_abilities"], name: "index_cards_on_activated_abilities", using: :gin
     t.index ["colors"], name: "index_cards_on_colors", using: :gin
