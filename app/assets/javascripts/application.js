1b9ec18c73cb8ebcd26ac489aefc5552ac8879a8
DanL12186 <danlrcn@hotmail.com>
2018-11-15 14:03:34 -0500

    moved edition mouseover script from view into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,25 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+  //awful, hacky script to get mouseover to change images. temporary. (yes, `edition` is a global variable.)
+
+  $(document).on("turbolinks:load", function() {
+    $(".edition_rare").on('mouseenter', function() {
+        edition = (this.src.match(/(?<=\/editions\/).*(?=\-)/) || this.src.match(/(?<=\/editions\/).*(?=\.)/))[0]
+        const src = `/assets/editions/${edition} rare.png`
+
+        this.src = src
+    }).on('mouseleave', function() {
+        this.src = `/assets/editions/${edition}.png`
+    });
+
+    $(".edition_uncommon").on('mouseenter', function() {
+        edition = (this.src.match(/(?<=\/editions\/).*(?=\-)/) || this.src.match(/(?<=\/editions\/).*(?=\.)/))[0]
+        const src = `/assets/editions/${edition} uncommon.png`
+    
+        this.src = src
+      }).on('mouseleave', function() {
+        this.src = `/assets/editions/${edition}.png`
+      });
+  })
