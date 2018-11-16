83d9a186cf1f192530c75462c66f68e7e99d2d4b
DanL12186 <danlrcn@hotmail.com>
2018-11-16 15:22:43 -0500

    Controller now updates card prices if they're older than 24h
    Leverages /models/cards.rb and application_helper.rb to achieve this

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,19 @@
 class CardsController < ApplicationController
+  include Cards
+  include ApplicationHelper
 
   def show
     @card = Card.find(params[:id])
+    if price_older_than_24_hours(@card.updated_at)
+      @card.update(price: [ get_mtgoldfish_price(@card.name, @card.edition), get_card_kingdom_price(@card.name, @card.edition) ])
+    end
   end
 
   def filter
     @cards = Card.all.select { | card | card.attributes.values.any? { | value | value == params[:filter] } }.sort_by(&:name)
+  end
+
+  def randhand
   end
 
   def index
