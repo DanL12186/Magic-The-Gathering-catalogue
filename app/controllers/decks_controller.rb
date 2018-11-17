462a5de101dad5aba9566f2ffb4dff6f6573aa40
DanL12186 <danlrcn@hotmail.com>
2018-11-17 16:48:20 -0500

    Gave decks#show instance variables needed in view. Refactor later.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,8 +1,12 @@
 class DecksController < ApplicationController
+  include DeckHandStats
 
   def show
     @deck = Deck.find(params[:id])
-    @deck_cards = @deck.cards.shuffle
+    @shuffled_deck_cards = shuffled_deck(@deck)
+    @sample_hand = sample_hand(@shuffled_deck_cards)
+    @next_eight_cards = next_eight_cards(@shuffled_deck_cards)
+    @stats_hash ||= hand_stats(@sample_hand)
   end
 
   def index
