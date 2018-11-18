53d0041d1e4b71af66405a21dbd6194886124508
DanL12186 <danlrcn@hotmail.com>
2018-11-18 14:30:32 -0500

    added dependent: :destroy to cards and decks so cards_decks will be destroyed if a corresponding card or deck is

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
   include Cards
 
   has_many :users_cards
-  has_many :decks_cards
+  has_many :decks_cards, dependent: :destroy
   has_many :collections_cards
 
   has_many :users, through: :users_cards
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,5 @@
 class Deck < ApplicationRecord
-  has_many :decks_cards
+  has_many :decks_cards, dependent: :destroy
   has_many :cards, through: :decks_cards
   has_many :card_types, through: :cards
   has_many :colors, through: :cards
