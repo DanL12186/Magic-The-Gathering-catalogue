dfd668fa5c5c61f0d9054c03851070b5cd7a5842
DanL12186 <danlrcn@hotmail.com>
2018-11-18 20:37:13 -0500

    Provided view with necessary statistics it needs on page refresh. Will refactor later to something less obtrusive.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -3,10 +3,15 @@ class DecksController < ApplicationController
 
   def show
     @deck = Deck.find(params[:id])
-    @shuffled_deck_cards = shuffled_deck(@deck)
+    @shuffled_deck_cards = shuffled_deck(@deck.cards)
     @sample_hand = sample_hand(@shuffled_deck_cards)
     @next_eight_cards = next_eight_cards(@shuffled_deck_cards)
-    @stats_hash ||= hand_stats(@sample_hand)
+    @stats_hash = hand_stats(@sample_hand)
+
+    @hand_frequencies = card_frequencies(@sample_hand)
+    @deck_frequencies = card_frequencies(@shuffled_deck_cards)
+
+    @multivar_args = multivar_geo_freq_args(@hand_frequencies, @deck_frequencies).values
   end
 
   def index
