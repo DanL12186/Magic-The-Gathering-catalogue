a18a5319fa41b1a2a074f26e5e3512072875036f
DanL12186 <danlrcn@hotmail.com>
2018-11-20 16:56:33 -0500

    Moved scripts into .js files; added a clear form  button.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,16 +17,48 @@
 //= require turbolinks
 //= require_tree .
 
-  $(document).on("turbolinks:load", function() {
-    $(".edition_rare, .edition_uncommon").on('mouseenter', function() {
-        const edition = this.parentElement.getAttribute('data-edition').replace('_', ' ')
-        ,     rarity  = this.parentElement.getAttribute('data-rarity');
-        console.log(this)
-        x = this
-        this.src = `/assets/editions/${edition} ${rarity}.png`;
-        
-    }).on('mouseleave', function() {
-        const edition = this.parentElement.getAttribute('data-edition').replace('_', ' ');
-        this.src = `/assets/editions/${edition}.png`;
+$(document).on("turbolinks:load", function() {
+  $(".edition_rare, .edition_uncommon").on('mouseenter', function() {
+    const edition = this.parentElement.getAttribute('data-edition').replace('_', ' ')
+    ,     rarity  = this.parentElement.getAttribute('data-rarity');
+
+    this.src = `/assets/editions/${edition} ${rarity}.png`;
+  }).on('mouseleave', function() {
+    const edition = this.parentElement.getAttribute('data-edition').replace('_', ' ');
+    this.src = `/assets/editions/${edition}.png`;
+  });
+
+//adds additional rows to table on hand_odds_calculator page
+  $('#add-card-inputs').on('click', function appendFields() {
+    const fieldCount = parseInt( $('#add-card-inputs').val() ) + 1
+    
+    $('#add-card-inputs').val(fieldCount)
+
+    $('table#odds-input-table').append(
+      `<tr>
+        <td> <input type=number name=in_deck_${fieldCount} id=in_deck_${fieldCount} class=input-sm> </input> </td>
+        <td> <input type=number name=in_hand_${fieldCount} id=in_hand_${fieldCount} class=input-sm> </input> </td>
+        <td> <input type=text name=card_name_${fieldCount} id=card_name_${fieldCount} class=input-sm> </input> </td>
+      </tr>`
+    )
+  });
+
+  //handles hand_odds_calculator submit form and appends result
+  $('form.calculate-odds').on('submit', function(event) {
+    event.preventDefault();
+
+    const serializedForm = $(this).serialize()
+    ,     response = $.post(`/decks/calculate_custom_hands_odds`, serializedForm);
+
+    response.done(function(resultAsFloat) {
+      $('#odds-results').html(resultAsFloat + "%");
     });
-  })
+  });
+
+  //clear input formx
+  $('button.clear-form').on('click', function(event) {
+    $('form.calculate-odds').trigger('reset')
+    //stops "please fill in this form" from triggering
+    event.preventDefault()
+  });
+})
