29225c37fac2f80e3ffc99ec5632b1fd65f5af7f
DanL12186 <danlrcn@hotmail.com>
2018-11-20 16:15:42 -0500

    added routes for sending odds calculation to be processed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "application#home"
 
+  post 'decks/calculate_custom_hands_odds' => 'decks#calculate_custom_hand_odds'
+  get '/hand_odds_calculator' => 'application#hand_odds_calculator'
+
   resources :cards
   resources :decks
-
-  get '/hand_odds_calculator' => 'application#hand_odds_calculator'
+  
   get ':filter' => 'cards#filter'
 
 end
