11a956bfc97f82bb621f8d196d2fbc1213c2d4f0
DanL12186 <danlrcn@hotmail.com>
2018-11-25 16:05:41 -0500

    updated code interacting w/scryfall API to properly handle revised, beta, etc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -254,11 +254,14 @@ mana_abbrev = {
     types = obj['type_line'].split
     types.delete_at(1)
     type = types.shift
+    edition = obj['set_name']
+    edition = edition.split.first if edition.match?(/Revised|Unlimited/)
+    edition = edition.split.last if edition.match?(/Beta|Alpha/)
     subtypes = []
     if types.size > 0
       subtypes = types
     end
-    Card.create(:name => obj['name'], edition: obj['set_name'], :hi_res_img => obj['image_uris']['large'], :cropped_img => obj['image_uris']['art_crop'], :reserved => obj['reserved'], :year => obj['frame'], :multiverse_id => obj['multiverse_ids'][0], :rarity => obj['rarity'].capitalize, power: obj['power'].try(:to_i), artist: obj['artist'], toughness: obj['toughness'].try(:to_i), mana: obj['mana_cost'].gsub(/\W/,'').split('').map { | x | mana_abbrev[x] || x }, card_type: type, subtypes: subtypes, flavor_text: obj['flavor_text'] )
+    Card.create(:name => obj['name'], edition: edition, :hi_res_img => obj['image_uris']['large'], :cropped_img => obj['image_uris']['art_crop'], :reserved => obj['reserved'], :year => obj['frame'], :multiverse_id => obj['multiverse_ids'][0], :rarity => obj['rarity'].capitalize, power: obj['power'].try(:to_i), artist: obj['artist'], toughness: obj['toughness'].try(:to_i), mana: obj['mana_cost'].gsub(/\W/,'').split('').map { | x | mana_abbrev[x] || x }, card_type: type, subtypes: subtypes, flavor_text: obj['flavor_text'] )
   end
   @url = @set['next_page']
   @set = JSON.parse(Nokogiri::HTML(open(@url)).text)
@@ -271,8 +274,11 @@ obj = JSON.parse(Nokogiri::HTML(open(@url)).text)
 types = obj['type_line'].split
 types.delete_at(1)
 type = types.shift
+edition = obj['set_name']
+edition = edition.split.first if edition.match?(/Revised|Unlimited/)
+edition = edition.split.last if edition.match?(/Beta|Alpha/)
 subtypes = []
 if types.size > 0
   subtypes = types
 end
-Card.create(:name => obj['name'], edition: obj['set_name'], :hi_res_img => obj['image_uris']['large'], :cropped_img => obj['image_uris']['art_crop'], :reserved => obj['reserved'], :year => obj['frame'], :multiverse_id => obj['multiverse_ids'][0], :rarity => obj['rarity'].capitalize, power: obj['power'].try(:to_i), artist: obj['artist'], toughness: obj['toughness'].try(:to_i), mana: obj['mana_cost'].gsub(/\W/,'').split('').map { | x | mana_abbrev[x] || x }, card_type: type, subtypes: subtypes, flavor_text: obj['flavor_text'] )
+Card.create(:name => obj['name'], edition: edition, :hi_res_img => obj['image_uris']['large'], :cropped_img => obj['image_uris']['art_crop'], :reserved => obj['reserved'], :year => obj['frame'], :multiverse_id => obj['multiverse_ids'][0], :rarity => obj['rarity'].capitalize, power: obj['power'].try(:to_i), artist: obj['artist'], toughness: obj['toughness'].try(:to_i), mana: obj['mana_cost'].gsub(/\W/,'').split('').map { | x | mana_abbrev[x] || x }, card_type: type, subtypes: subtypes, flavor_text: obj['flavor_text'] )
