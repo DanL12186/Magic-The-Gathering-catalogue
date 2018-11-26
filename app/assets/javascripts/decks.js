b8ad2436f6e6bbcbe60558408a8e364ad020041e
DanL12186 <danlrcn@hotmail.com>
2018-11-25 21:25:37 -0500

    + => parseInt

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -2,7 +2,7 @@ $(document).on("turbolinks:load", function() {
 
   $('.deck-display').on('click', function() {
     const currentCardNumber = $("#card-counter")
-    ,     numCards = +currentCardNumber.html().trim().match(/\d+$/)[0]
+    ,     numCards = parseInt(currentCardNumber.html().trim().match(/\d+$/)[0])
     
     cardCount = this.parentElement.getAttribute('value')
     this.parentElement.setAttribute('value', ++cardCount)
