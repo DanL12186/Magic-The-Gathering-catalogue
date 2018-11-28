8cfcec3f9b0dc9659a2ee83351e4d546a21c3131
DanL12186 <danlrcn@hotmail.com>
2018-11-28 01:09:19 -0500

    Created new page to begin to split up deck show page(s)
    Added a doughnut chart to visualize the breakdown of card types in the
    deck. Included same mana curve chart which will be removed from show
    page.Added necessary variables to controller

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -1,5 +1,6 @@
 $(document).on("turbolinks:load", function() {
 
+  //clicking hrough cards on deck show page
   $('.deck-display').on('click', function() {
     const currentCardNumber = $("#card-counter")
     ,     numCards = parseInt(currentCardNumber.html().trim().match(/\d+$/)[0])
@@ -10,4 +11,45 @@ $(document).on("turbolinks:load", function() {
     currentCardNumber.html(`Card ${cardCount} of ${numCards}`)
     this.remove()
   });
+
+  //pie chart for displaying deck card-types breakdown on deck overview page
+  function pieChartLoader() {
+    const jsonData = $("#pieChartContainer").data('json'),
+          [ labels, frequencies ] = [ Object.keys(jsonData).map(str=> str.replace('_', ' ')), Object.values(jsonData) ],
+          totalCards = frequencies.reduce((a,b)=> a+b),
+          dataPoints = []
+
+
+    for (let i = 0; i < labels.length; i++) {
+      if (!['medium creatures', 'small creatures', 'large creatures', 'nonbasic lands'].includes(labels[i])) {
+        const percent = Math.round(100 * frequencies[i]/totalCards),
+              label   = labels[i].replace(labels[i][0], labels[i][0].toUpperCase())
+        dataPoints.push( 
+          { label: label, 
+            y: frequencies[i], 
+            legendText: `${label}: ${percent}%` 
+          }
+        )
+      }
+    }
+    
+    const options = {
+      backgroundColor: "silver",
+      title: {
+        fontFamily: "MagicMedieval",
+        text: "Deck Breakdown",
+      },
+        animationEnabled: true,
+        data: [
+          {
+            type: "doughnut",
+            dataPoints: dataPoints,
+            showInLegend: true
+          }
+        ]
+      };
+    $("#pieChartContainer").CanvasJSChart(options);
+  };
+  $("#pieChartContainer").ready(pieChartLoader);
+  $("#pieChartContainer").on('turbolinks:load', pieChartLoader)
 })
--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,6 @@
 class DecksController < ApplicationController
   include DeckHandStats
+  before_action :set_deck, only: [:show, :overview]
 
   def calculate_custom_hand_odds
     @deck_size = params[:deck_size].to_i
@@ -7,11 +8,10 @@ class DecksController < ApplicationController
     @multivar_args = get_param_stats(params).values
     @answer = multivariate_hypergeometric_distribution(@deck_size, @cards_drawn, *@multivar_args)
 
-    render json: @answer, status: 201
+    render json: @answer
   end
 
   def show
-    @deck = Deck.find(params[:id])
     @shuffled_deck_cards = shuffled_deck(@deck.cards)
     @sample_hand = sample_hand(@shuffled_deck_cards)
     @next_eight_cards = next_eight_cards(@shuffled_deck_cards)
@@ -23,8 +23,21 @@ class DecksController < ApplicationController
     @multivar_args = multivar_geo_freq_args(@hand_frequencies, @deck_frequencies).values
   end
 
+  def overview
+    #some of these don't make sense for the overview (e.g. shuffled deck). Review all variables/methods later.
+    @deck_frequencies = card_frequencies(@deck.cards)
+    @shuffled_deck_cards = shuffled_deck(@deck.cards)
+    @stats_object = hand_stats(@shuffled_deck_cards).to_json
+  end
+
   def index
     @decks = Deck.all
   end
 
+  private
+
+  def set_deck
+    @deck = Deck.find(params[:id])
+  end
+  
 end
