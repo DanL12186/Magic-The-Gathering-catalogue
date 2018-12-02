b770d3b8d9adbbb47007fca4509ba6201ff35f14
DanL12186 <danlrcn@hotmail.com>
2018-12-01 19:34:09 -0500

    updated to jquery3 for security and perf reasons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
+//= require jquery3
 //= require activestorage
 //= require bootstrap-sprockets
 //= require turbolinks
