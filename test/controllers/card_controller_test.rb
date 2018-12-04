4ef2113359be8bb42dfd12d35b6ff04a2af71c49
DanL12186 <danlrcn@hotmail.com>
2018-12-04 18:48:56 -0500

    added a few basic tests

--- test/controllers/card_controller_test.rb
+++ test/controllers/card_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class CardControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get cards_path
+
+    assert_equal "index", @controller.action_name
+    assert_match "Partial Matches:", @response.body
+  end
+
+  test "Should get root path as home" do    
+    get root_path
+    
+    assert_equal "home", @controller.action_name
+    assert_match "Find a Card", @response.body
+  end
+
+  test "Should load the appropriate card and cache prices by saving to db once scraped" do
+    card = Card.new(name: "Mox Diamond", edition: "Stronghold")
+    card.save
+
+    start = Time.now
+    get card_path(card)
+    finish = Time.now
+
+    uncached_page_load = finish - start
+
+    start = Time.now
+    get card_path(card)
+    finish = Time.now
+
+    cached_page_load = finish - start
+
+    puts "Uncached: #{uncached_page_load}, Cached: #{cached_page_load}"
+    assert (cached_page_load * 100) < (uncached_page_load)
+    assert @response.body.include?("Mox Diamond")    
+  end
 end
