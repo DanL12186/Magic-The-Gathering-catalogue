d8798dcf94f667fd14f046ff148387de5b9d6230
DanL12186 <danlrcn@hotmail.com>
2018-12-05 15:11:43 -0500

    added routes for a more advanced search

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root "application#home"
 
-  post '/decks/calculate_custom_hand_odds' => 'decks#calculate_custom_hand_odds'
   get '/hand_odds_calculator' => 'application#hand_odds_calculator'
+  post '/decks/calculate_custom_hand_odds' => 'decks#calculate_custom_hand_odds'
+
+  get '/cards/find_by_properties'
+  post '/cards/filter_search' => 'cards#filter_search'
 
   resources :cards
   resources :decks
