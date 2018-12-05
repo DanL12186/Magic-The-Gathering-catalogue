282fc2c218952cdaa9f3850fdbbafb3a592bd9c5
DanL12186 <danlrcn@hotmail.com>
2018-12-05 15:24:40 -0500

    added code to add found cards to page. This format is temporary.

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -33,4 +33,31 @@ $(document).on('turbolinks:load', function() {
   $("li.card").on('click', function() {
     this.children[1].remove()
   });
+
+
+  //populate /cards/find_by_properties with found cards
+  $("form.find_by_properties").on('submit', function(event) {
+    event.preventDefault()
+        const serializedForm = $(this).serialize()
+        ,     response = $.post(`/cards/filter_search`, serializedForm);
+    
+    response.done((cards) => {
+      const html = cards.map(card=> {
+        const cardClass = card.edition === 'Alpha' ? 'card_img alpha' : 'card_img'
+        
+        return `<div>
+          <h1> ${card.name} </h1>
+          <h3 data-edition= ${card.edition.toLowerCase().replace(/ /g,'_')} data-rarity=${card.rarity.toLowerCase()}> 
+            Edition: <a href="/${card.edition}"> ${card.edition} </a> <img src="/assets/editions/${card.edition.toLowerCase()}" class= edition_${card.rarity.toLowerCase()} height=2% width=2% >
+          </h3>
+          
+      <div class=card_img_div> <a href="/cards/${card.id}/"> <img src="${card.img_url}" class=${cardClass} > </a> </div>            
+          
+        </div>`
+      })
+
+      document.getElementById("find_cards").innerHTML = html.join('');
+    })
+  })
+
 })
