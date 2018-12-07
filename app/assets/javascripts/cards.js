1d8e1ed0b2daec64579c2b6a7532fd029696ab22
DanL12186 <danlrcn@hotmail.com>
2018-12-07 13:53:17 -0500

    fomatting

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -45,15 +45,17 @@ $(document).on('turbolinks:load', function() {
       const html = cards.map(card=> {
         const cardClass = card.edition === 'Alpha' ? 'card_img alpha' : 'card_img'
         
-        return `<div>
-          <h1> ${card.name} </h1>
-          <h3 data-edition= ${card.edition.toLowerCase().replace(/ /g,'_')} data-rarity=${card.rarity.toLowerCase()}> 
-            Edition: <a href="/${card.edition}"> ${card.edition} </a> <img src="/assets/editions/${card.edition.toLowerCase()}" class= edition_${card.rarity.toLowerCase()} height=2% width=2% >
-          </h3>
-          
-      <div class=card_img_div> <a href="/cards/${card.id}/"> <img src="${card.img_url}" class=${cardClass} > </a> </div>            
-          
-        </div>`
+        return( 
+          `<div>
+            <h1> ${card.name} </h1>
+            <h3 data-edition= ${card.edition.toLowerCase().replace(/ /g,'_')} data-rarity=${card.rarity.toLowerCase()}> 
+              Edition: <a href="/${card.edition}"> ${card.edition} </a> <img src="/assets/editions/${card.edition.toLowerCase()}" class= edition_${card.rarity.toLowerCase()} height=2% width=2% >
+            </h3>
+            
+            <div class=card_img_div> <a href="/cards/${card.id}/"> <img src="${card.img_url}" class=${cardClass} > </a> </div>            
+            
+          </div>`
+        )
       })
 
       document.getElementById("find_cards").innerHTML = html.join('');
