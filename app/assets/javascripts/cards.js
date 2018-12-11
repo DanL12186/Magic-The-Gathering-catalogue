10aa008a18eef22e265bfc709c620832a973a348
DanL12186 <danlrcn@hotmail.com>
2018-12-11 01:35:00 -0500

    Added methods to create buttons for sorting when cards are displayed, and code to sort by name, multiverseID, and price.
    May need cleaning up later

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -42,18 +42,15 @@ $(document).on('turbolinks:load', function() {
     const serializedForm = $(this).serialize()
     ,     response = $.post(`/cards/filter_search`, serializedForm);
     
-    response.done(cards => {
-      if (cards === null) {
-        document.getElementById("find_cards").innerHTML = 'Please Select One or More Options'
-        return;
-      }
-      const html = cards.map(card=> {
+    function generateCardsHTML(cards) {
+      return cards.map(card=> {
         const cardClass = card.edition === 'Alpha' ? 'card_img alpha' : 'card_img'
-        const thumbnail = (card.hi_res_img || card.img_url).replace(/large/,'small')
+        ,     thumbnail = (card.hi_res_img || card.img_url).replace(/large/,'small')
+
         return( 
           `<div class = 'col-sm-3'>
             <h3 data-edition= ${card.edition.toLowerCase().replace(/ /g,'_')} data-rarity=${card.rarity.toLowerCase()}> 
-              ${card.name} <img src="/assets/editions/${card.edition.toLowerCase()}" class= edition_${card.rarity.toLowerCase()} height=4.5% width=4.5% >
+              ${card.name} <img src="/assets/editions/${card.edition.toLowerCase()}" class= edition_${card.rarity.toLowerCase()} height=5% width=5% >
             </h3>
             
             <div class=card_img_div> <a href="/cards/${card.id}/"> <img src="${thumbnail}" class=${cardClass} style="width:146px; height: 204px;"> </a> </div>            
@@ -61,9 +58,50 @@ $(document).on('turbolinks:load', function() {
           </div>`
         )
       })
+    };
 
-      document.getElementById("find_cards").innerHTML = html.join('') || "No results found";
-    })
-  })
+    response.done(cards => {
 
-})
+      if (cards === null) {
+        document.getElementById("find_cards").innerHTML = 'Please Select One or More Options'
+        return;
+      }
+
+      const html = generateCardsHTML(cards);
+
+      document.getElementById("find_cards").innerHTML = html.join('') || "No results found"
+
+      //create buttons to sort newly displayed card results
+      document.getElementById("sort_by_name").innerHTML = `<button>Sort By Name</button>`
+      document.getElementById("sort_by_id").innerHTML = `<button>Sort By Multiverse ID</button>`
+      document.getElementById("sort_by_price").innerHTML = `<button>Sort By Price</button>`
+
+
+      $("#sort_by_name").on('click', function(event) {
+        event.preventDefault();
+
+        const sortedCards = generateCardsHTML(cards.sort((a,b)=> a.name[0].charCodeAt() - b.name[0].charCodeAt()))
+        document.getElementById("find_cards").innerHTML = sortedCards.join('');
+      });
+
+      $("#sort_by_id").on('click', function(event) {
+        event.preventDefault();
+        
+        const sortedCards = generateCardsHTML(cards.sort((a,b)=> a.multiverse_id - b.multiverse_id))
+        document.getElementById("find_cards").innerHTML = sortedCards.join('');
+      });
+
+      $("#sort_by_price").on('click', function(event) {
+        event.preventDefault();
+
+        const sortedCards = generateCardsHTML(cards.sort((a,b)=> {
+          const [priceA, priceB] = [a, b].map(card=> parseFloat(card.price[1].match(/\d+\,*\d*\.*\d*/) || []))
+          return priceB - priceA;
+        }));
+
+        document.getElementById("find_cards").innerHTML = sortedCards.join('');
+      });
+    });
+  });
+
+});
