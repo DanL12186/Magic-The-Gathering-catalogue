c4e2384f80cf9e019c5d532cb398823b70b75f42
DanL12186 <danlrcn@hotmail.com>
2018-12-16 18:11:24 -0500

    added missing declaration (let), added break in HTML so image won't jump up when div is removed

--- app/assets/javascripts/decks.js
+++ app/assets/javascripts/decks.js
@@ -2,13 +2,13 @@ $(document).on("turbolinks:load", function() {
 
   //clicking hrough cards on deck show page
   $('.deck-display').on('click', function() {
+    let   cardCount = this.parentElement.getAttribute('value');
     const currentCardNumber = $("#card-counter")
-    ,     numCards = parseInt(currentCardNumber.html().trim().match(/\d+$/)[0])
-    
-    cardCount = this.parentElement.getAttribute('value')
+    ,     numCards = parseInt(currentCardNumber.html().trim().match(/\d+$/)[0]);
+
     this.parentElement.setAttribute('value', ++cardCount)
     
-    currentCardNumber.html(cardCount <= numCards ? `Card ${cardCount} of ${numCards}` : '')
+    currentCardNumber.html(cardCount <= numCards ? `Card ${cardCount} of ${numCards}` : '<br>')
 
     this.remove();
   });
@@ -18,11 +18,10 @@ $(document).on("turbolinks:load", function() {
     const jsonData = $("#pieChartContainer").data('json'),
           [ labels, frequencies ] = [ Object.keys(jsonData).map(str=> str.replace('_', ' ')), Object.values(jsonData) ],
           totalCards = frequencies.reduce((a,b)=> a+b),
-          dataPoints = []
-
+          dataPoints = [];
 
     for (let i = 0; i < labels.length; i++) {
-      if (!['medium creatures', 'small creatures', 'large creatures', 'nonbasic lands'].includes(labels[i])) {
+      if (['creatures', 'lands', 'spells', 'artifacts'].includes(labels[i])) {
         const percent = Math.round(100 * frequencies[i]/totalCards),
               label   = labels[i].replace(labels[i][0], labels[i][0].toUpperCase());
 
