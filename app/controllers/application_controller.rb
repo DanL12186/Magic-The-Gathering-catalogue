aa2962ba1be59d909c495dda5d5d8c4bb18e4d20
DanL12186 <danlrcn@hotmail.com>
2018-12-30 23:25:10 -0500

    quick fix for @current_user not persisting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    session[:user_id] ? @@current_user ||= User.find(session[:user_id]) : @@current_user = nil
   end
 
   def logged_in?
