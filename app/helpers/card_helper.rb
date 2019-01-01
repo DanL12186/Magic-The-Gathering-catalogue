f5ef81912164869dda7a752c9794a4c5dcab7e9a
DanL12186 <danlrcn@hotmail.com>
2019-01-01 17:33:26 -0500

    Fixed issue with sets containing ":" and new core sets

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -7,7 +7,8 @@ module CardHelper
   end
 
   def edition_filename(rarity, edition)
-    rarity == "Common" ? "editions/#{edition.downcase}" : "editions/#{edition.downcase} #{rarity.downcase}"
+    file = rarity == "Common" ? "editions/#{edition.downcase}" : "editions/#{edition.downcase} #{rarity.downcase}"
+    file.delete(':')
   end
 
   def is_modern_or_basic_land?(card)
@@ -37,21 +38,31 @@ module CardHelper
     (Time.now - last_updated) > 24.hours
   end
 
+  def add_prices_to_all
+    cards = Card.where(price: [])
+    cards.each do | card | 
+      args = [card.name, card.edition]
+      prices = [get_mtgoldfish_price(*args), get_card_kingdom_price(*args), get_tcg_player_price(*args) ]
+      card.update(price: prices)
+    end
+  end
+
   ##################################### Links and Scraping #####################################
 
   def scrape_page_if_exists(url)
     Thread.new do 
       begin
-        Nokogiri::HTML(open(url)) 
+        Nokogiri::HTML(open(url))
       rescue OpenURI::HTTPError => error
         raise error unless error.message.match?("Not Found")
       end
     end
   end
 
+
   def mtgoldfish_url(card_name, card_set)
     set = (card_set.match?(/Alpha|Beta/) ? ("Limited Edition #{card_set}") : card_set.match?(/Rev|Unl/) ? ("#{card_set} Edition") : (card_set))
-          .gsub(' ', '+').delete("'")
+          .gsub(' ', '+').delete("':")
     name = I18n.transliterate(card_name).gsub(' ', '+').delete(",.:;'")
 
     "https://www.mtggoldfish.com/price/#{set}/#{name}#paper"
@@ -65,9 +76,13 @@ module CardHelper
     price ? price.delete(',') : 'N/A'
   end
 
+
   def card_kingdom_url(card_name, card_set)
-    set = (card_set == 'Revised') ? ('3rd-edition') : (card_set == 'Fourth Edition') ? '4th-edition' : card_set.gsub(' ', '-').downcase.delete("'")
-    set = set.match(/\d{4}/) ? "#{set.match(/\d+/)[0]}-core-set" : set
+    puts card_set
+    set = (card_set == 'Revised') ? ('3rd-edition') : (card_set == 'Fourth Edition') ? '4th-edition' : card_set.gsub(' ', '-').downcase.delete("':")
+    set = set.match(/201[0-5]/) ? "#{set.match(/\d+/)[0]}-core-set" : set
+    set = "Ravnica" if card_set.match?("Ravnica: City of Guilds")
+    puts set
     name = I18n.transliterate(card_name.downcase).gsub(' ','-').delete(",.:;'")
 
     "https://www.cardkingdom.com/mtg/#{set}/#{name}"
@@ -83,7 +98,7 @@ module CardHelper
 
 
   def tcg_player_url(card_name, card_set)
-    set = card_set.gsub(' ', '-').downcase
+    set = card_set.gsub(' ', '-').downcase.delete(':')
     set += '-edition' if card_set.match?(/Alpha|Beta|Unl|Rev/)
     name = I18n.transliterate(card_name.downcase).gsub(' ', '-').delete(",.:;'")
 
@@ -98,6 +113,7 @@ module CardHelper
     price ? price.delete('$,') : 'N/A'
   end
 
+
   def gatherer_link(multiverse_id)
     "http://gatherer.wizards.com/Pages/Card/Details.aspx?printed=true&multiverseid=#{multiverse_id}"
   end
@@ -108,5 +124,4 @@ module CardHelper
     "https://www.ebay.com/sch/i.html?&_nkw=#{set}+#{name}"
   end
   #######################################################################################################
-  
 end
