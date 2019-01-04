236421f343558e364af82f1adba536d20335b2fa
DanL12186 <danlrcn@hotmail.com>
2019-01-04 18:47:30 -0500

    refactored and cleaned up a bit

--- app/assets/javascripts/cards/cards.js
+++ app/assets/javascripts/cards/cards.js
@@ -18,53 +18,53 @@ $(document).on('turbolinks:load', function() {
 
   //switch transform images to high-res Scryfall image (672x936) from original low-res image (223x310)   
   $("#card_show_img_face, #card_show_img_back").on('click', function() {
-    const cardFace    = (this.id === "card_show_img_face") ? (this) : (document.getElementById("card_show_img_back"))
-    ,     cardBack    = (cardFace.id) === "card_show_img_face" ? document.getElementById("card_show_img_back") : document.getElementById("card_show_img_face")
+    const cardFace    = (this.id === 'card_show_img_face') ? (this) : (document.getElementById('card_show_img_back'))
+    ,     cardBack    = (cardFace.id) === 'card_show_img_face' ? document.getElementById('card_show_img_back') : document.getElementById('card_show_img_face')
     ,     originalSrc = cardFace.getAttribute('original_src')
     ,     hiResImgUrl = cardFace.getAttribute('img_url')
     ,     originalTwinSrc = cardBack.getAttribute('original_src')
     ,     hiResImgUrlTwin = cardBack.getAttribute('img_url');
 
-    const pricesDiv     = $(".price")[0]
+    const pricesDiv     = document.getElementById('prices')
     ,     flipContainer = document.getElementsByClassName('flip-card-inner')[0]
 
     if (zoomed) {
       flipContainer.style.width = '223px';
       cardFace.src = originalSrc;
       cardBack.src = originalTwinSrc
-      cardFace.style.width = "223px";
-      cardFace.style.height = "310px";
+      cardFace.style.width = '223px';
+      cardFace.style.height = '310px';
       cardBack.style.width = '223px';
       cardBack.style.height = '310px';
-      pricesDiv.style = "transition: 2s; float: left";
+      pricesDiv.style = 'transition: 2s; float: left';
       zoomed = false;
     } else { 
       flipContainer.style.width = '500px';
       cardFace.src = hiResImgUrl;
       cardBack.src = hiResImgUrlTwin;
-      cardFace.style.width = "502px";
-      cardFace.style.height = "700px";
-      cardBack.style.width = "502px";
-      cardBack.style.height = "700px";
-      pricesDiv.style = "transition: 1.5s; margin-left: 57%; margin-top: 3.5%;"
+      cardFace.style.width = '502px';
+      cardFace.style.height = '700px';
+      cardBack.style.width = '502px';
+      cardBack.style.height = '700px';
+      pricesDiv.style = 'transition: 1.5s; margin-left: 57%; margin-top: 3.5%;'
       zoomed = true
     }
   });
 
   //switch to high-res Scryfall image (672x936) from original low-res image (223x310) 
-  $("#card_show_img").on('click', function() {
+  $('#card_show_img').on('click', function() {
     const originalSrc = this.getAttribute('original_src')
     ,     hiResImgUrl = this.getAttribute('img_url')
 
     if (zoomed) {
       this.src = originalSrc;
-      this.style.width = "223px";
-      this.style.height = "310px";
+      this.style.width = '223px';
+      this.style.height = '310px';
       zoomed = false
     } else { 
-      this.src = hiResImgUrl; //.replace("large", "normal") for smaller image (488x680 @ ~55-60% file size)
-      this.style.width = "502px";
-      this.style.height = "700px";
+      this.src = hiResImgUrl; //.replace('large', 'normal') for smaller image (488x680 @ ~55-60% file size)
+      this.style.width = '502px';
+      this.style.height = '700px';
       zoomed = true
     }
   });
@@ -90,7 +90,7 @@ $(document).on('turbolinks:load', function() {
   //flip dual-sided cards over
   let transformed; 
 
-  $("#transform").on('click', function() {
+  $('#transform').on('click', function() {
     const div = document.getElementsByClassName('flip-card-inner')[0]
     if (!transformed) {
       div.style.transform = 'rotateY(180deg)';
@@ -103,8 +103,8 @@ $(document).on('turbolinks:load', function() {
 
   //updates DOM on card show page with new prices if older than 24hrs or prices don't exist.
   if (document.getElementById('card-kingdom')) {
-    const stale = $(".price").data('stale')
-    ,     id = $('.price')[0].id;
+    const stale = $('#prices').data('stale')
+    ,     id = $('#prices').data('id');
 
     if (stale) {
       const response = $.post(`/cards/update_prices?id=${id}`)
@@ -112,19 +112,18 @@ $(document).on('turbolinks:load', function() {
       response.done(card=> {
         //card.price is an array of mtgoldfish, card kingdom, and tcg player prices. Updates DOM if price has changed.
         for (let i = 0; i < 3; i++) {
-          const oldPrice = $("h4 span")[i].innerText.replace(/[\$,]/g,''),
+          const oldPrice = $('.price')[i].innerText.replace(/[\$,]/g,''),
                 newPrice = card.price[i];
-
+          
           if (oldPrice !== newPrice) {
-            const spanID = $("h4")[i+1].id,
-                  price  = card.price[i],
-                  selector = `h4#${spanID} span`;
+            const spanID = $('#prices h4')[i].id,
+                  priceSpan = $(`h4#${spanID} span`);
 
-            $(selector).fadeOut(750).fadeIn(750)
+            priceSpan.fadeOut(750).fadeIn(750);
 
             setTimeout(() => {
-              $("h4 span")[i].innerText = newPrice !== 'N/A' ? '$' + numberWithDelimiter(price) : 'N/A'
-            }, 750)
+              $('.price')[i].innerText = newPrice !== 'N/A' ? '$' + numberWithDelimiter(newPrice) : 'N/A'
+            }, 750);
           };
         }
       })
@@ -145,7 +144,7 @@ $(document).on('turbolinks:load', function() {
   })
 
   //remove popover after leaving page
-  $("li.card").on('click', function() {
+  $('li.card').on('click', function() {
     this.children[1].remove()
   });
 
