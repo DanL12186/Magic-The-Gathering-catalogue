a8c9d7f330eb676c3373e83b027b95a3d5e41668
DanL12186 <danlrcn@hotmail.com>
2019-01-03 20:57:46 -0500

    commented out needless code

--- vendor/assets/javascripts/lazyload.js
+++ vendor/assets/javascripts/lazyload.js
@@ -89,12 +89,12 @@
 
             self.loaded = false;
 
-            /* If no src attribute given use data:uri. */
-            if ($self.attr("src") === undefined || $self.attr("src") === false) {
-                if ($self.is("img")) {
-                    $self.attr("src", settings.placeholder);
-                }
-            }
+            // /* If no src attribute given use data:uri. */
+            // if ($self.attr("src") === undefined || $self.attr("src") === false) {
+            //     if ($self.is("img")) {
+            //         $self.attr("src", settings.placeholder);
+            //     }
+            // }
 
             /* When appear is triggered load original image. */
             $self.one("appear", function() {
