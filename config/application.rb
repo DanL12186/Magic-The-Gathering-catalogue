f237d26ed251e3debbfa409d3e01adc2a193a426
DanL12186 <danlrcn@hotmail.com>
2019-01-03 20:58:22 -0500

    added explanatory comment

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MagicTheGatheringPostgresql
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    #enable text compression
     config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
