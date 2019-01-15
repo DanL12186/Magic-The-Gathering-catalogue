dbb7c1f62752975c9872ed2be11af32a69e12c5b
DanL12186 <danlrcn@hotmail.com>
2019-01-14 22:58:45 -0500

    temporary controller actions while routing and models are being redone; filtering artists, editions and colors through separate routes and controllers now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,14 @@ class ApplicationController < ActionController::Base
   def redirect_if_logged_in
     redirect_to root_path if logged_in?
   end
+
+  #temporary before sets are their own class
+  def edition
+    @cards = Card.where(edition: params[:edition]).sort_by(&:multiverse_id)
+  end
+
+  def artist
+    @cards = Card.where(artist: params[:artist]).sort_by { | card | [card.name, card.multiverse_id] }.uniq.sort_by { | card | [ card.multiverse_id, card.color ] }
+  end
   
 end
