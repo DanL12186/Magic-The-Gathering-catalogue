ca6daae93fd405c8e31e6150edd855a27d40b82c
DanL12186 <danlrcn@hotmail.com>
2019-01-29 16:00:40 -0500

    limit home page to 50 cards to further speed up loads

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,7 +1,7 @@
 class HomePageController < ApplicationController
 
   def home
-    @iconic_cards = Card.where(iconic: true).limit(75)
+    @iconic_cards = Card.where(iconic: true).limit(50)
   end
 
 end
