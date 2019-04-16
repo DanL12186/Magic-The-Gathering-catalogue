e103e218e5a344bd58cb6f7f0fad55ed6a96e2ef
DanL12186 <danlrcn@hotmail.com>
2019-04-15 21:15:12 -0400

    added private build_deck method for creating deck and associated cards (decks_cards)

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -4,9 +4,14 @@ class DecksController < ApplicationController
   before_action :set_deck, only: [:show, :overview]
   before_action :set_shuffled_deck, only: [:show, :overview]
   before_action :deny_unauthorized_deck_access, only: [:show, :overview, :edit, :update, :destroy]
+  before_action :build_deck, only: [:create]
 
   def new
     @deck = Deck.new
+  end
+
+  def create
+    @deck.valid? ? (redirect_to decks_path) : (render :new)
   end
 
   def calculate_custom_hand_odds
@@ -50,6 +55,36 @@ class DecksController < ApplicationController
 
   def deny_unauthorized_deck_access
     redirect_to root_path unless logged_in? && @deck.user_id == current_user.id
+  end
+
+
+  #create the deck 'manually' here.
+  def build_deck
+    cards_with_quantities = params[:decks_cards][:list].split(/ \r\n/)
+    
+    deck_name = deck_params[:name]
+
+    @deck = Deck.new( {name: deck_name, user_id: current_user.id } )
+    @deck.save
+
+    return nil unless @deck.id
+
+    @decks_cards = cards_with_quantities.map do | card_string | 
+      edition_match = card_string.match(/[a-z]+(?=\])/i)
+      
+      copies, name = card_string.sub(/ \[[a-z]+\]/i, '').split(/x /)
+      
+      #find cheapest card variant if no edition/set is specified
+      card_id = find_specific_or_cheapest_card_variant_id(name, edition_match[0])
+    
+      { copies: copies.to_i, deck_id: @deck.id, card_id: card_id }
+    end
+
+    DecksCard.create(@decks_cards)
+  end
+
+  def deck_params
+    params.require(:deck).permit(:name, :user_id, :colors, :card_types, decks_cards_attributes: [:card_id, :copies])
   end
 
 end
