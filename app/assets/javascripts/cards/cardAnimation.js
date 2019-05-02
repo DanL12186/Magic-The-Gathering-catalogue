22686e01be98c26d577965beeda55880a55a7bf1
DanL12186 <danlrcn@hotmail.com>
2019-05-02 15:37:00 -0400

    Cleaned up somewhat and commented code for easier reading
    Should continue to fix this up

--- app/assets/javascripts/cards/cardAnimation.js
+++ app/assets/javascripts/cards/cardAnimation.js
@@ -4,7 +4,6 @@ $(document).on('turbolinks:load', function() {
   const pricesDiv = document.getElementById('prices');
   let   zoomed;
 
-  //may src.replace('large', 'normal') for smaller 488x680 image @ ~55-60% file size
   //should probably just addClass/removeClass and set class to do work instead of JS
   function zoomOut(element) {
     pricesDiv.style = 'transition: 2s; float: left';
@@ -22,7 +21,7 @@ $(document).on('turbolinks:load', function() {
     zoomed = true;
   }
 
-  //switch transform images to high-res Scryfall image (672x936) from original low-res image (223x310)
+  //switch transform face/back  images to high-res Scryfall image (672x936) from original low-res images (223x310)
   $("#card_show_img_face, #card_show_img_back").on('click', function() {
     const cardFace = this.id.includes('face') ? this : document.getElementById('card_show_img_face')
     ,     cardBack = this.id.includes('back') ? this : document.getElementById('card_show_img_back')
@@ -45,9 +44,9 @@ $(document).on('turbolinks:load', function() {
     zoomed ? zoomOut(this) : zoomIn(this)
   });
 
+  //rotate split-view cards 90 or -90 degrees
   let rotated;
 
-  //rotate split-view cards 90 or -90 degrees
   $("#rotate").on('click', function() {
     const angle = this.getAttribute('data-rotate')
     ,     image = document.getElementById('card_show_img');
@@ -64,59 +63,66 @@ $(document).on('turbolinks:load', function() {
     rotated = !rotated
   });
 
-  function fadeSwitchImage() {
-    const button = $("#transform")[0],
-          croppedDiv = $("#cropped-img-div"),
+  //Fades out cropped face image, fades in cropped back image.
+  function fadeSwitchCroppedImage() {
+    const croppedDiv = $("#cropped-img-div"),
           croppedImg = document.getElementById('cropped-img'),
           faceCropURL = croppedImg.attributes.original_src.value,
           backCropURL = croppedImg.attributes.reverse_src.value;
 
     croppedDiv.fadeOut(750).fadeIn(750);
-    button.setAttribute('disabled', true);
-
+    
     setTimeout(()=> {
       croppedImg.src = transformed ? faceCropURL : backCropURL
-      button.removeAttribute('disabled');
       transformed = !transformed
     }, 750)
   }
 
-  const flipCardOver = cardDiv => { cardDiv.style.transform = `rotateY(${180-(transformed ? 180 : 0)}deg)` }
+  const flipCardOver = () => { cardDiv.style.transform = `rotateY(${180-(transformed ? 180 : 0)}deg)` }
 
+  function temporarilyDisableButton(button, duration = 750) {
+    button.setAttribute('disabled', true);
+    
+    setTimeout(()=> {
+      button.removeAttribute('disabled');
+    }, duration);
+  }
+
+  const cardNameSpan = $("#name_and_edition");
+  let   faceCardName,
+        backCardName;
+
+  if (document.getElementById("transform")) {
+    faceCardName = cardNameSpan[0].innerText.replace(/\(.+/, '').trim();
+    backCardName = this.getElementsByClassName("col-sm-3 flip-card")[0].getAttribute('data-flipname');
+  };
+
+  //change flip card name to the face that's currently showing
+  function fadeSwitchName() {
+    const currentFaceName = transformed ? backCardName : faceCardName,
+          currentBackName = transformed ? faceCardName : backCardName;
+
+    cardNameSpan.fadeOut(750).fadeIn(750)
+
+    setTimeout(()=> {
+      cardNameSpan[0].innerHTML = cardNameSpan[0].innerHTML.replace(currentFaceName, currentBackName)
+    }, 750);
+  };
+
+  //flip dual-sided cards over and change name and cropped image with a fade effect
   let transformed,
       cardDiv;
 
-  //flip dual-sided cards over and change cropped image with a fade effect to match flipped side
   $('#transform').on('click', function() {
     if (cardDiv === undefined) {
       cardDiv = document.getElementsByClassName('flip-card-inner')[0];
     }
 
-    flipCardOver(cardDiv)
-    fadeSwitchImage()
-    fadeSwitchName()
+    temporarilyDisableButton(this, 750);
+
+    flipCardOver();
+    fadeSwitchName();
+    fadeSwitchCroppedImage();
   });
-
-  let faceCardName,
-      backCardName;
-
-  if (document.getElementById("transform")) {
-    const cardNameSpan = $("#name_and_edition")
-    
-    faceCardName = cardNameSpan[0].innerText.replace(/\(.+/, '').trim()
-    backCardName = this.getElementsByClassName("col-sm-3 flip-card")[0].getAttribute('data-flipname')
-
-    //change flip card name to the face that's currently showing
-    function fadeSwitchName() {
-      const currentFaceName = transformed ? backCardName : faceCardName,
-            currentBackName = transformed ? faceCardName : backCardName;
-
-      cardNameSpan.fadeOut(750).fadeIn(750)
-
-      setTimeout(()=> {
-        cardNameSpan[0].innerHTML = cardNameSpan[0].innerHTML.replace(currentFaceName, currentBackName)
-      }, 750)
-    }
-  };
 
 });
