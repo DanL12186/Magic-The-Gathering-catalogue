712a4c1c824a78e23c94e4712b9f4809784d341b
DanL12186 <daniel.lempesis@gmail.com>
2019-08-12 16:02:40 -0400

    Add a couple basic tests to home controller

--- spec/controllers/home_page_controller_spec.rb
+++ spec/controllers/home_page_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe HomePageController, type: :controller do
+  render_views
 
+  describe "GET root_path" do            
+    # it "assigns @teams" do
+    #   get :root_path
+    #   expect(assigns(:teams)).to eq([team])
+    # end
+
+    it "renders the home page template and doesn't show the home page link" do
+      get :home
+
+      expect(response.body).to match /MTG Catalogue/
+      expect(response.body).to_not match /Home/      
+    end
+  end
 end
