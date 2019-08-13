40f8be91c5ab7532583c73ff7383f2ce69559c25
DanL12186 <daniel.lempesis@gmail.com>
2019-08-13 16:55:44 -0400

    Make a helper method to calculate value of a user's collection

--- app/helpers/collection_helper.rb
+++ app/helpers/collection_helper.rb
@@ -1,2 +1,9 @@
 module CollectionHelper
+  def calculate_collection_value(collection)
+    collection.collections_cards.map do | collection_card | 
+      card = collection_card.card
+      card_kingdom_price = card.prices[1].to_f || 0
+      collection_card.copies * card_kingdom_price
+    end.sum 
+  end
 end
