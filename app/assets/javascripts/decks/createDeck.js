4d64223a134fc71d8691fda141f9a3b5b231443e
DanL12186 <daniel.lempesis@gmail.com>
2019-08-13 16:06:25 -0400

    Formatting improvements

--- app/assets/javascripts/decks/createDeck.js
+++ app/assets/javascripts/decks/createDeck.js
@@ -5,12 +5,12 @@ $(document).on('turbolinks:load', function() {
     this.getElementById('deckCardFind').value = ''
   }
 
-  let cardNames,
-      lastMatch;
+  let cardNames;
+  let lastMatch;
 
   //for some reason, form and head auth tokens don't match until refresh, this is a fix
-  const railsAuthenticityToken = $('head [name=csrf-token]')[0].content,
-        formAuthenticityToken  = $("#new_deck input")[1]
+  const railsAuthenticityToken = $('head [name=csrf-token]')[0].content;
+  const formAuthenticityToken  = $("#new_deck input")[1];
 
   $('#deckCardFind').on('focus', () => {
     if (!cardNames) {
@@ -22,9 +22,9 @@ $(document).on('turbolinks:load', function() {
   function addCardToList(event, cardName, copies) {
     event.preventDefault();
 
-    const addedCardList = document.getElementById('decks_cards_list')
-    ,     datalist      = document.getElementById('deckBuildSearch')
-    ,     input         = document.getElementById('deckCardFind')
+    const addedCardList = document.getElementById('decks_cards_list'),
+          datalist      = document.getElementById('deckBuildSearch'),
+          input         = document.getElementById('deckCardFind');
 
     addedCardList.innerHTML += `${copies}x ${cardName} \n`
     
@@ -36,10 +36,10 @@ $(document).on('turbolinks:load', function() {
   //$(document)[0].body.innerHTML += `<img src= "https://img.scryfall.com/cards/small/en/chr/81.jpg">`
   $('#deckCardFind').on('keyup', event => {
     if (event.target.value) {
-      const userEntry       = event.target.value.toLowerCase()
-      ,     matches         = cardNames.filter(name=> name.toLowerCase().startsWith(userEntry))
-      ,     datalist        = document.getElementById('deckBuildSearch')
-      ,     firstTenMatches = matches.slice(0,10);
+      const userEntry       = event.target.value.toLowerCase(),
+            matches         = cardNames.filter(name=> name.toLowerCase().startsWith(userEntry)),
+            datalist        = document.getElementById('deckBuildSearch'),
+            firstTenMatches = matches.slice(0, 10);
 
       //only update on change
       if (lastMatch !== firstTenMatches.toString()) {
@@ -51,8 +51,8 @@ $(document).on('turbolinks:load', function() {
 
   //this also triggers just by hitting enter as a mouse event
   $("#addCard").on('click', event => {
-    const cardOption = document.getElementById('deckBuildSearch').options[0]
-    ,     copies     = document.getElementById('decks_cards_copies').value;
+    const cardOption = document.getElementById('deckBuildSearch').options[0];
+    const copies     = document.getElementById('decks_cards_copies').value;
 
     if (cardOption) {
       const cardName = cardOption.innerText.trim()
@@ -66,8 +66,8 @@ $(document).on('turbolinks:load', function() {
   });
 
   $("#addLand").on('click', event => {
-    const landName = document.getElementById('decks_cards_land_name').value
-    ,     copies   = document.getElementById('decks_cards_land_copies').value;
+    const landName = document.getElementById('decks_cards_land_name').value;
+    const copies   = document.getElementById('decks_cards_land_copies').value;
 
     addCardToList(event, landName, copies)
   });
