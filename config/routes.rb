7c82ebcc0f288d9ac0b135e3ecc74d6d8d5ea525
DanL12186 <daniel.lempesis@gmail.com>
2019-08-24 20:16:33 -0400

    Remove open_booster../:edition route. Add post route for user to select set

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,9 @@ Rails.application.routes.draw do
   get '/about' => 'application#about'
 
   get '/editions' => 'editions#index'
-  get '/editions/open_booster_pack/:edition' => 'editions#open_booster_pack'
+
+  get '/editions/open_booster_pack/' => 'editions#open_booster_pack'
+  post '/editions/open_booster_pack/' => 'editions#open_booster_pack'
 
   get '/hand_odds_calculator' => 'application#hand_odds_calculator'
   post '/decks/calculate_custom_hand_odds' => 'decks#calculate_custom_hand_odds'
