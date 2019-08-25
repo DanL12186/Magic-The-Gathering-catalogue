922a6768b3844737c09136e29bf02163513b4205
DanL12186 <daniel.lempesis@gmail.com>
2019-08-24 20:12:13 -0400

    Add ordered_editions. Set edition name to either what the user selected, or Revised by default. Expose edition (edition => @edition) to view.

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -7,14 +7,16 @@ class EditionsController < ApplicationController
   end
 
   def open_booster_pack
-    @edition_name = params[:edition]
+    @ordered_editions = Edition.where.not(cards_per_pack: nil).pluck(:name).sort_by(&SET_NAMES_IN_CHRONOLOGICAL_ORDER)
+    
+    @edition_name = params[:edition] || "Revised"
     @booster_pack = generate_booster(@edition_name)
   end
 
   private
 
     def generate_booster(edition_name)
-      edition   = Edition.find_by(name: edition_name)
+      @edition  = Edition.find_by(name: edition_name)
       all_cards = Card.select(:name, :prices, :edition, :img_url, :rarity, :card_number).where(edition: edition_name)      
       mana      = Set.new(['Swamp', 'Island', 'Mountain', 'Forest', 'Plains'])
 
@@ -26,15 +28,15 @@ class EditionsController < ApplicationController
       commons   = cards.select { | card | card.rarity == 'Common' }
       uncommons = cards.select { | card | card.rarity == 'Uncommon' }
       rares     = cards.select { | card | card.rarity == 'Rare' }
-      mythics   = cards.select { | card | card.rarity == 'Mythic' } if edition.mythics?
+      mythics   = cards.select { | card | card.rarity == 'Mythic' } if @edition.mythics?
 
       booster_pack = []
 
-      booster_pack << commons.sample(edition.commons_per_pack)
-      booster_pack << uncommons.sample(edition.uncommons_per_pack)
+      booster_pack << commons.sample(@edition.commons_per_pack)
+      booster_pack << uncommons.sample(@edition.uncommons_per_pack)
 
       #1/8 chance of getting a mythic instead of rare
-      if edition.mythics? && rand(1..8) == 8
+      if @edition.mythics? && rand(1..8) == 8
         booster_pack << mythics.sample
       else
         booster_pack << rares.sample
