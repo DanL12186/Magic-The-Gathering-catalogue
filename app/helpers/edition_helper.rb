6bd0a5dfde96c9f99187dfc64b00063c1a79f65d
DanL12186 <daniel.lempesis@gmail.com>
2019-08-24 20:18:50 -0400

    Add a method to determine whether a foil can be/is generated for a given card

--- app/helpers/edition_helper.rb
+++ app/helpers/edition_helper.rb
@@ -3,5 +3,9 @@ module EditionHelper
   def get_edition_image_file(edition_name, rarity)
     "editions/#{edition_name.downcase} #{rarity.downcase}".sub(/ common/, '')
   end
+
+  def roll_foil?(edition)
+    Date.parse(edition.release_date) >= Date.parse('1999-02-15') && rand(1..67) == 67
+  end
   
 end
