903e5cfc2138f20071637a66b3e88ca3a19c102d
DanL12186 <daniel.lempesis@gmail.com>
2019-08-26 15:29:33 -0400

    Cleaned up #generate_booster and added comments

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -16,11 +16,13 @@ class EditionsController < ApplicationController
 
   private
 
+    #Generate a booster as accurately as possible. Old sets like Alpha - Revised had lands in common slots; Arabian Nights had a common mountain.
+    #Some more modern sets have a fixed one land per pack (not yet implemented)
     def generate_booster(edition_name)
-      all_cards = Card.select(:name, :prices, :foil_prices, :edition, :img_url, :rarity, :card_number).where(edition: edition_name)      
-      mana      = Set.new(['Swamp', 'Island', 'Mountain', 'Forest', 'Plains'])
+      booster_pack = []
 
-      cards     = edition_name.match?(/Arabian|Alpha|Beta|Unlimited|Revised/) ? all_cards.to_a : all_cards.reject { | card | mana.include?(card.name) }
+      all_cards = Card.select(:name, :prices, :foil_prices, :edition, :img_url, :rarity, :card_number).where(edition: edition_name).to_a
+      cards     = edition_name.match?(/Arab|Alpha|Beta|Unlim|Revise/) ? all_cards : all_cards.reject { | card | ApplicationHelper::LANDS.include?(card.name) }
 
       #ignore the back face of flip cards
       cards.reject! { | card | card.card_number.match?('b') }
@@ -30,17 +32,11 @@ class EditionsController < ApplicationController
       rares     = cards.select { | card | card.rarity == 'Rare' }
       mythics   = cards.select { | card | card.rarity == 'Mythic' } if @edition.mythics?
 
-      booster_pack = []
-
       booster_pack << commons.sample(@edition.commons_per_pack)
       booster_pack << uncommons.sample(@edition.uncommons_per_pack)
 
-      #1/8 chance of getting a mythic instead of rare
-      if @edition.mythics? && rand(1..8) == 8
-        booster_pack << mythics.sample
-      else
-        booster_pack << rares.sample
-      end
+      #1/8 chance of getting a mythic instead of rare in sets which have mythic rarity cards
+      booster_pack << (@edition.mythics? && rand(1..8) == 8 ? mythics.sample : rares.sample)
       
       booster_pack.flatten
     end
