b81b9e510a80689d7f78012635e2a1094a55bb49
DanL12186 <DanL12186@users.noreply.github.com>
2020-03-29 14:07:12 -0500

    Fix error that occurred with modal if login was incorrect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: session_params[:name])
-    session[:user_id] = @user.id  if  @user && @user.authenticate(session_params[:password])
-    if !!session[:user_id]
+    session[:user_id] = @user.id if @user&.authenticate(session_params[:password])
+    if session[:user_id]
       redirect_to root_path
     else
-      @user = User.new(name: session_params[:name])
-      flash[:error] = "Username and password don't match"
-      render :new
+      render partial: "layouts/login_modal", notice: "Username and password don't match"
     end
   end
 
