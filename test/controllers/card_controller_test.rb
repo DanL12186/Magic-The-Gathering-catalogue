072ed3a59dfb08a72827de3720e99a04faf5ce69
DanL12186 <DanL12186@users.noreply.github.com>
2020-04-11 17:52:48 -0500

    fix/update tests

--- test/controllers/card_controller_test.rb
+++ test/controllers/card_controller_test.rb
@@ -1,63 +1,18 @@
 require 'test_helper'
 
 class CardControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get '/cards'
+  test "should get reserved list" do
+    get '/cards/reserved_list'
 
-    assert_equal "index", @controller.action_name
-    assert_match "No results found.", @response.body
+    assert_equal "reserved_list", @controller.action_name
+
+    assert_match "Reserved List", @response.body
   end
 
   test "Should get root path as home" do    
     get root_path
     
     assert_equal "home", @controller.action_name
-    assert_match "Find a Card", @response.body
-  end
-
-  #disable below test until Rails isn't using a thread to load the page before scraping and persisting to db. 
-  test "Shouldn't update load the appropriate card and cache prices by saving to db once scraped" do
-    puts "Scraping card data"
-    card = Card.new(name: "Mox Diamond", edition: "Stronghold", price: ['$195.00', '$200.00', '$206.92'])
-    card.save
-
-    start = Time.now
-    get card_path(card)
-    finish = Time.now
-
-    uncached_page_load = finish - start
-
-    start = Time.now
-    get card_path(card)
-    finish = Time.now
-
-    cached_page_load = finish - start
-
-    puts "Uncached: #{uncached_page_load}s, Cached: #{cached_page_load}s"
-    assert (cached_page_load * 100) < (uncached_page_load)
-    assert @response.body.include?("Mox Diamond")    
+    assert_match "Card Search", @response.body
   end
 end
-
-  ##disable below test until Rails isn't using a thread to load the page before scraping and persisting to db. 
-  # test "Should load the appropriate card and cache prices by saving to db once scraped" do
-  #   puts "Scraping card data"
-  #   card = Card.new(name: "Mox Diamond", edition: "Stronghold")
-  #   card.save
-
-  #   start = Time.now
-  #   get card_path(card)
-  #   finish = Time.now
-
-  #   uncached_page_load = finish - start
-
-  #   start = Time.now
-  #   get card_path(card)
-  #   finish = Time.now
-
-  #   cached_page_load = finish - start
-
-  #   puts "Uncached: #{uncached_page_load}s, Cached: #{cached_page_load}s"
-  #   assert (cached_page_load * 100) < (uncached_page_load)
-  #   assert @response.body.include?("Mox Diamond")    
-  # end
--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -2,51 +2,53 @@ require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
 
-  test "Card can't be saved without an edition and a name" do
-    card = Card.new(name: "Fritada", artist: "Michelangelo")
+  test "Card can't be saved without an edition, name, and multiverse_id" do
+    #no edition
+    card = Card.new(name: "Fritada", artist: "Michelangelo", colors: ["Blue"], multiverse_id: 999)
     assert_not card.save
 
+    #no multiverse_id
+    card = Card.new(name: "Fritada", artist: "Michelangelo", colors: ["Blue"])
+    assert_not card.save
+
+    #no name
     assert_raises(NoMethodError) do
-      card = Card.new(edition: "Sistine", artist: "Michelangelo")
+      card = Card.new(edition: "Sistine", artist: "Michelangelo", colors: ["Blue"], multiverse_id: 999)
       card.save
     end
   end
 
   test "Card is saved with an edition and a name" do
-    card = Card.new(name: "Fritada", edition: "Starch", artist: "Michelangelo")
+    card = Card.new(name: "Fritada", edition: "Starch", artist: "Michelangelo", colors: ["Blue"], multiverse_id: 999)
     
     assert card.save
   end
   
   test "Card can access all users who 'own' it" do
-    card = Card.new(name: "Potato", edition: "Snacks")
+    card = Card.new(name: "Potato", edition: "Snacks", colors: ["White"], multiverse_id: 111)
     card.save
     
-    user1 = User.new(name: "Joe", email: "celtic@ts.net", password: 'butwhatifwe')
+    user1 = User.new(name: "Joe", email: "celtic@ts.net", password: "butwhatifwe")
     user1.save
 
-    user2 = User.new(name: "Barack", email: "renegade@dj.org", password: 'no,joe')
+    user2 = User.new(name: "Barack", email: "renegade@dj.org", password: "no,joe..")
     user2.save
 
     UsersCard.create(card_id: card.id, user_id: user1.id)
     UsersCard.create(card_id: card.id, user_id: user2.id)
-    
-    assert Card.find(card.id).users.map(&:name) == ["Joe", "Barack"]
+
+    assert card.users.map(&:name) == ["Joe", "Barack"]
   end
 
   test "Card can access all decks it belongs to when supplied with necessary information" do
-    card = Card.new(name: "Potato", edition: "Snacks")
-    card.save
-    
-    user = User.new(name: "Barack", email: "renegade@dj.org", password: 'no,joe')
-    user.save
+    card = Card.new(name: "Potato", edition: "Snacks", colors: ["White"], multiverse_id: 112)
+    user = User.new(name: "Barack", email: "renegade@dj.org", password: 'no,joe..')
 
-    deck1 = Deck.new(name: "Paper", user_id: user.id)
-    deck1.save
-    
-    deck2 = Deck.new(name: "Tiger", user_id: user.id)
-    deck2.save
+    [card, user].each(&:save)
 
+    deck1 = Deck.create(name: "Paper", user_id: user.id)
+    deck2 = Deck.create(name: "Tiger", user_id: user.id)
+    
     DecksCard.create(card_id: card.id, deck_id: deck1.id)
     DecksCard.create(card_id: card.id, deck_id: deck2.id)
     
