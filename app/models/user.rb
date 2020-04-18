facbcc8df3f8f5a2c2f89b380f4b7dbd24f38be7
DanL12186 <DanL12186@users.noreply.github.com>
2020-04-18 15:21:07 -0500

    Don't require email, but enforce uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_many :collections
 
   validates :password, presence: true, length: { minimum: 8 }
-  validates :name, :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: true
 end
