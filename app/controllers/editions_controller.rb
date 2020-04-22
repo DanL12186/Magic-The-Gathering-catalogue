d22b3b00023faf4b621aeddafa2409ea88cc1b02
DanL12186 <DanL12186@users.noreply.github.com>
2020-04-22 16:09:42 -0500

    Allow user to filter out commons and uncommons

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -9,20 +9,21 @@ class EditionsController < ApplicationController
   def open_booster_pack
     @ordered_editions = Edition.where.not(cards_per_pack: nil).pluck(:name).sort_by(&SET_NAMES_IN_CHRONOLOGICAL_ORDER)
     
-    @edition_name = params[:edition] || "Revised"
-    @edition  = Edition.find_by(name: @edition_name)
-    @booster_pack = generate_booster(@edition_name)
+    @hide_commons   = params[:hide_commons] == "1"
+    @hide_uncommons = params[:hide_uncommons] == "1"
+    @edition        = Edition.find_by(name: params[:edition] || "Revised")
+    @booster_pack   = generate_booster()
   end
 
   private
 
     #Generate a booster as accurately as possible. Old sets like Alpha - Revised had lands in common slots; Arabian Nights had a common mountain.
     #Some more modern sets have a fixed one land per pack (not yet implemented)
-    def generate_booster(edition_name)
+    def generate_booster
       booster_pack = []
 
-      all_cards = Card.select(:name, :prices, :foil_prices, :edition, :img_url, :rarity, :card_number, :frame).where(edition: edition_name).to_a
-      cards     = edition_name.match?(/Arab|Alpha|Beta|Unlim|Revise/) ? all_cards : all_cards.reject { | card | ApplicationHelper::LANDS.include?(card.name) }
+      all_cards = Card.select(:name, :prices, :foil_prices, :edition, :img_url, :rarity, :card_number, :frame).where(edition: @edition.name).to_a
+      cards     = @edition.name.match?(/Arab|Alpha|Beta|Unlim|Revise/) ? all_cards : all_cards.reject { | card | ApplicationHelper::LANDS.include?(card.name) }
 
       #ignore the back face of flip cards
       cards.reject! { | card | card.card_number.match?('b') }
@@ -32,8 +33,8 @@ class EditionsController < ApplicationController
       rares     = cards.select { | card | card.rarity == 'Rare' }
       mythics   = cards.select { | card | card.rarity == 'Mythic' } if @edition.mythics?
 
-      booster_pack << commons.sample(@edition.commons_per_pack)
-      booster_pack << uncommons.sample(@edition.uncommons_per_pack)
+      booster_pack << commons.sample(@edition.commons_per_pack) unless @hide_commons
+      booster_pack << uncommons.sample(@edition.uncommons_per_pack) unless @hide_uncommons
 
       #1/8 chance of getting a mythic instead of rare in sets which have mythic rarity cards
       booster_pack << (@edition.mythics? && SecureRandom.random_number(1..8) == 8 ? mythics.sample : rares.sample)
