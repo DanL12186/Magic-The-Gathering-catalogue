198cfd793abf910feb197f20836e6b91ef24734a
DanL12186 <DanL12186@users.noreply.github.com>
2020-05-06 11:21:39 -0500

    fix Ikoria name

--- app/models/concerns/card_sets.rb
+++ app/models/concerns/card_sets.rb
@@ -151,7 +151,7 @@ module CardSets
       "Core Set 2020" => "M20",
       "Throne of Eldraine" => "ELD",
       "Theros Beyond Death" => "THB",
-      "Ikoria: Lair of the Behemoths" => "IKO",
+      "Ikoria: Lair of Behemoths" => "IKO",
     },
     "Core Sets" => {
       "Eighth Edition" => "8ED",
@@ -318,7 +318,7 @@ module CardSets
     "War of the Spark" => "WAR",
     "Throne of Eldraine" => "ELD",
     "Theros Beyond Death" => "THB",
-    "Ikoria: Lair of the Behemoths" => "IKO",
+    "Ikoria: Lair of Behemoths" => "IKO",
 
     #core sets
     "Magic 2010" => "M10",
