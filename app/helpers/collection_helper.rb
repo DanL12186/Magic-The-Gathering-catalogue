bf0e803856d95a9887286442fd5f52976bbaf9d1
DanL12186 <DanL12186@users.noreply.github.com>
2020-05-06 10:58:27 -0500

    parse collection value into monetary format. link to cards in view.

--- app/helpers/collection_helper.rb
+++ app/helpers/collection_helper.rb
@@ -1,17 +1,21 @@
 module CollectionHelper
   def calculate_collection_value(collection)
-    collection.collections_cards.map do | collection_card | 
-      card = collection_card.card
-      card_kingdom_price = card.prices[1].to_f || 0
-      collection_card.copies * card_kingdom_price
-    end.sum.round(2)
+    value = collection.collections_cards.map do | collection_card | 
+              card = collection_card.card
+              card_kingdom_price = card.prices[1].to_f || 0  
+              collection_card.copies * card_kingdom_price
+            end.sum.round(2)
+
+    number_with_delimiter(value)
   end
 
 
   def display_collection(collection)
     collection.collections_cards.map do | collection_card |
       card = collection_card.card
-      "<li> #{collection_card.copies}x #{card.name} - $#{card.prices[1]} </li>"
+      name, edition, price, copies = [card.name, card.edition, card.prices[0], collection_card.copies]
+
+      "<li>#{copies}x #{link_to(name, card_path(edition, name))} - $#{price}</li>"
     end.join.html_safe
   end
 
