bc2627d438215e73baddc913a2dbdcdbcfecb077
DanL12186 <DanL12186@users.noreply.github.com>
2020-05-21 10:53:44 -0500

    Reload current page on successful login rather than sending user back to homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,9 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: session_params[:name])
     session[:user_id] = @user.id if @user&.authenticate(session_params[:password])
+
     if session[:user_id]
-      redirect_to root_path
+      redirect_back(fallback_location: root_path)
     else
       render json: 'Authentication failed'.to_json
     end
