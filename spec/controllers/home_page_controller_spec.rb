6dd05ecdfd34b53757d00d3a03225612ef120c6f
DanL12186 <DanL12186@users.noreply.github.com>
2020-06-05 15:53:54 -0500

    Check to see iconic cards are displayed on homepage

--- spec/controllers/home_page_controller_spec.rb
+++ spec/controllers/home_page_controller_spec.rb
@@ -4,17 +4,26 @@ require 'pry'
 RSpec.describe HomePageController, type: :controller do
   render_views
 
-  describe "GET root_path" do            
-    # it "assigns @teams" do
-    #   get :root_path
-    #   expect(assigns(:teams)).to eq([team])
-    # end
+  describe "GET root_path" do
+    Card.create(
+      name: "Air Elemental", edition: "Alpha", artist: "Richard Thomas", card_type: "Creature", rarity: "Uncommon", 
+      multiverse_id: 94, mana: ["3", "Blue", "Blue"], subtypes: ["Elemental"], color: "Blue", colors: ["Blue"], 
+      img_url: "https://cdn1.mtggoldfish.com/images/gf/Air%2BElemental%2B%255BLEA%255D.jpg", iconic: true
+    )
 
-    it "renders the home page template and doesn't show the home page link" do
+    it "renders the home page template" do
       get :home
-
       expect(response.body).to match /MTG Catalogue/
-      expect(response.body).to_not match /Home/      
+    end
+
+    it "should display iconic cards" do
+      get :home
+      expect(response.body).to have_css('.card_img')
+    end
+
+    it "doesn't show the home page link" do
+      get :home
+      expect(response.body).to_not match /Home/
     end
   end
 end
