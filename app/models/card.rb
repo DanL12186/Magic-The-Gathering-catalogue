e28c65dc7c350d884db6949606e6ce93f44fc634
DanL12186 <DanL12186@users.noreply.github.com>
2020-07-26 11:39:13 -0500

    Use JSON.generate for faster initial responses

--- app/models/card.rb
+++ app/models/card.rb
@@ -41,20 +41,22 @@ class Card < ApplicationRecord
     end
   end
 
-  #piggybacks on .all_card_names method; same return value only in JSON form; for card search autocomplete feature
+  #for card search autocomplete feature; piggybacks on .all_card_names method and converts to JSON
   def self.all_card_names_json
     cache_key = "all_unique_card_names_json#{Time.now.day}"
 
     Rails.cache.fetch(cache_key, expires_in: 24.hours) do
-      Card.all_card_names.to_s
+      JSON.generate(Card.all_card_names)
     end
   end
 
-  #for the autocomplete feature for selecting cards for a collection by name and edition. returns json (array.to_s)
+  #for the autocomplete feature for selecting cards for a collection by name and edition. returns json
   def self.all_card_and_edition_names_json
     cache_key = "all_card_names_with_editions#{Time.now.day}"
-    names_and_editions = Rails.cache.fetch(cache_key, expires_in: 24.hours) do
-      Card.pluck(:name, :edition).to_s
+    
+    Rails.cache.fetch(cache_key, expires_in: 24.hours) do
+      cards = Card.pluck(:name, :edition)
+      JSON.generate(cards)
     end
   end
 
