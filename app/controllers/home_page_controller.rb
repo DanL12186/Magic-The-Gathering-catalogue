edf270f384fe5f0eb8d9b5c30da5f7b1c9795089
DanL12186 <DanL12186@users.noreply.github.com>
2020-08-03 09:53:34 -0500

    cache iconic_cards, reinstate 25 card limit

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,7 +1,11 @@
 class HomePageController < ApplicationController
 
   def home
-    @iconic_cards = Card.where(iconic: true)
+    cache_key = "iconic_cards#{Time.now.hour}"
+
+    @iconic_cards = Rails.cache.fetch(cache_key, expires_in: 10.minutes) do
+      Card.where(iconic: true).limit(25).to_a
+    end
   end
 
 end
